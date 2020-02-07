da39483d6037b0d90530cdbeccb773a4fe3b957c
phillipj1514 <phillipjunior95@gmail.com>
2020-02-07 17:21:09 -0500

    Project lab 2 profile card creation

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime 
 
 
 ###
@@ -22,12 +23,21 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Phillip")
 
+
+@app.route('/profile/')
+def profile():
+    date_joined = datetime.date(2020, 2, 6)
+    return render_template('profile.html', date = format_date_joined(date_joined))
+
+def format_date_joined(date):
+    return date.strftime("%B, %Y")
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
